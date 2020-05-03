60bf67a4bc434d4c65cf69d2ea6622c4cf37fa5b
Yury Komarov <33968020+yurymkomarov@users.noreply.github.com>
2020-05-03 23:57:24 +0300

    update code format style

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "results" {
   value = merge(
-  local.default_vars,
-  local.workspace_vars,
-  local.custom_vars
+    local.default_vars,
+    local.workspace_vars,
+    local.custom_vars
   )
 }
